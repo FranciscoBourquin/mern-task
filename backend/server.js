3e876df2c5c8de338d519958a4e3afa1ffd21e4e
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-10-05 14:06:08 -0300

    server.js corrected

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,9 @@ app.use(errorHandler);
 const node = process.env.NODE_ENV;
 
 if (node === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res)=> {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
     })
 }
 
