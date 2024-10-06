7ec79fcd97a80a8f6b88e4d57f7906911a9363ef
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-10-05 21:37:07 -0300

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const node = process.env.NODE_ENV;
 if (node === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
     app.get("*", (req, res)=> {
-        res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"))
     })
 }
 
